d809811b15d38c4838e9cb34ee5b6b12f80404a5
kmspofizre <kmspofizre13@yandex.ru>
2025-02-06 12:34:00 +0300

    hotfixes with tokens

--- refresh.py
+++ refresh.py
@@ -7,13 +7,7 @@ def refresh_tokens():
     dotenv_path = ".env"
 
     r = requests.post("https://api.traffic.online/api/v1/auth/token", json=login_password, headers=headers_auth)
-
-    d_refresh = {
-        "grant_type": "refresh_token",
-        "refresh_token": r.json().get("refresh_token")
-    }
-    r1 = requests.post("https://api.traffic.online/api/v1/auth/token", json=d_refresh, headers=headers_auth)
-    data = r1.json()
+    data = r.json()
     dotenv.set_key(dotenv_path, 'API_KEY', data.get("access_token"))
     dotenv.set_key(dotenv_path, 'API_REFRESH', data.get("refresh_token"))
-    return r1.status_code
+    return r.status_code
