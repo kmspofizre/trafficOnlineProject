514961a97b7064a0aa7809a82b2d788f5f24a2ea
kmspofizre <kmspofizre13@yandex.ru>
2025-02-08 19:35:19 +0300

    utils and new classes extended

--- ShippingBooker.py
+++ ShippingBooker.py
@@ -1,15 +1,28 @@
 from pydantic import BaseModel
 from typing import Dict
-from requests import Session
-from constants import headers_post
+from requests import Session, Response
+from constants import headers_post, post_application_query
 
 
 class ShippingBooker(BaseModel):
     post_header: Dict[str, str]
     session: Session
 
-    def __init__(self, session: Session, api_key:str):
+    def __init__(self, session: Session, api_key: str):
         self.post_header = headers_post
         headers_post['Authorization'] = f"Bearer {api_key}"
         self.session = session
         super().__init__()
+
+    def book_shipping(self, shipping_id: int) -> Response:
+        booking_response = self.session.post(
+            f"{post_application_query}{shipping_id}/reserve",
+            headers=headers_post)
+        return booking_response
+
+    def update_headers(self, api_key: str) -> bool:
+        try:
+            self.post_header['Authorization'] = f"Bearer {api_key}"
+            return True
+        except KeyError as e:
+            return False
--- TrafficBot.py
+++ TrafficBot.py
@@ -1,26 +1,30 @@
 from pydantic import BaseModel
-from typing import Dict
+from typing import List
 from ShippingBooker import ShippingBooker
 from ShippingGetter import ShippingGetter
 from requests import Session
+from utils import get_ids
 
 
 class TrafficBot(BaseModel):
 
-    post_header: Dict[str, str]
     shipping_getter: ShippingGetter
     shipping_booker: ShippingBooker
     session: Session
+    shipping_ids: List[str]
+    data_filename: str
 
-    def __init__(self, api_key: str):
+    def __init__(self, api_key: str, data_filename: str):
         self.session = Session()
         self.shipping_getter = ShippingGetter(self.session, api_key)
         self.shipping_booker = ShippingBooker(self.session, api_key)
+        self.data_filename = data_filename
+        self.shipping_ids = get_ids(data_filename)
         super().__init__()
 
     def refresh_api_key(self, api_key: str):
-        self.get_header['Authorization'] = f"Bearer {api_key}"
-        self.post_header['Authorization'] = f"Bearer {api_key}"
+        self.shipping_getter.update_headers(api_key)
+        self.shipping_booker.update_headers(api_key)
 
     def poll(self):
         pass
